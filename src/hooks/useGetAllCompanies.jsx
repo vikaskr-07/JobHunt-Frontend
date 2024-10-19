aa0f05786818be3bc829c92e653de5b18bf89d3c
vikaskr-07 <vikaskum7ar@gmail.com>
2024-10-19 23:36:03 +0530

    companies.jsx

--- src/hooks/useGetAllCompanies.jsx
+++ src/hooks/useGetAllCompanies.jsx
@@ -1,25 +1,33 @@
-import { setCompanies} from '@/redux/companySlice'
-import { COMPANY_API_END_POINT} from '@/utils/constant'
-import axios from 'axios'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { setCompanies } from '@/redux/companySlice';
+import { COMPANY_API_END_POINT } from '@/utils/constant';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 const useGetAllCompanies = () => {
     const dispatch = useDispatch();
-    useEffect(()=>{
+    
+    useEffect(() => {
         const fetchCompanies = async () => {
             try {
-                const res = await axios.get(`${COMPANY_API_END_POINT}/get`,{withCredentials:true});
+                const res = await axios.get(`${COMPANY_API_END_POINT}/get`, { withCredentials: true });
                 console.log('called');
-                if(res.data.success){
+                
+                if (res.data.success) {
                     dispatch(setCompanies(res.data.companies));
                 }
             } catch (error) {
-                console.log(error);
+                if (error.response) {
+                    console.error('Error response:', error.response.data);
+                    console.error('Error status:', error.response.status);
+                } else {
+                    console.error('Error message:', error.message);
+                }
             }
-        }
+        };
+        
         fetchCompanies();
-    },[])
-}
+    }, [dispatch]);
+};
 
-export default useGetAllCompanies
+export default useGetAllCompanies;
